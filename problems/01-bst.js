83ca8ab69de98c99078dbc2b657121b40d4c89a7
Laura <lkzailac@gmail.com>
2021-03-07 10:22:44 -0500

    testing insert

--- problems/01-bst.js
+++ problems/01-bst.js
@@ -9,12 +9,20 @@ class TreeNode {
 class BST {
     // Initialize an empty binary search tree
     constructor() {
-        // Your code here
+        this.root = null;
+
     }
 
     // Insert a node into the binary search tree
     insert(val, currentNode=this.root) {
-        // Your code here
+        if(!this.root) {
+            this.root = new TreeNode(val);
+        }
+        if(currentNode.val < val) {
+            currentNode.left = insert(val, currentNode)
+        } else {
+            currentNode.right = insert(val, currentNode)
+        }
     }
 
     // Perform a recursive search through the binary search tree
